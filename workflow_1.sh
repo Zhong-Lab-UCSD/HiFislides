a590330eebcd2870ffc2a13750081367290e6498
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-04 14:07:25 -0700

    Update workflow_1.sh

--- workflow_1.sh
+++ workflow_1.sh
@@ -81,9 +81,7 @@ samtools view -S -b $sam --threads 16 > $bam 2>>anye
 # getgenefromgtf.pl Mus_musculus.GRCm39.105.gtf ENSMUSG > genensmusg105.b 2>>anye
 bedtools intersect -a $bam -b genensmusg105.b -wb -bed > $genicreadfile
 
-# nohup GeneRankedBySpatialReadCount.pl L2R1bytileL00$L\_ak$k\o $genicreadfile > GenelistBySpatialRdCount.L 2>GenelistBySpatialRdCount.errmsg &
 nohup testGeneToTileByL2_step0.pl L2R1bycoordL00$L\_ak$k\o $genicreadfile > MatrixL00$L\_geneXtile.o 2>MatrixL00$L\_geneXtile.errmsg &
-# let n=n+1
 
 n1=`ps x | grep "testGeneToTileByL2_step0.pl" | wc -l`
 while [ $n1 -gt 1 ]
