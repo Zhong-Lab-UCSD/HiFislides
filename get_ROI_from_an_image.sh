966d9b6c0ea7d591bdb12afd442dbb23ae7529d1
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-14 08:37:57 -0700

    Update get_ROI_from_an_image.sh

--- get_ROI_from_an_image.sh
+++ get_ROI_from_an_image.sh
@@ -2,3 +2,6 @@
 
 # below is one example:
 select the region which contain our ROI and copied and saved as a new image (356 × 368 pixels)
+
+# on Mac, installed python 3.10.4 and also opencv
+# open the python script by IDLE and give the correct path of the image file (in this case, the 356 x 368 image)
