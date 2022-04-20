3edbc0c0cf2b1017ca03a7ddad027f99d8fca9ba
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-20 11:47:33 -0700

    Update workflow_2_MiniSeq.sh

--- workflow_2_MiniSeq.sh
+++ workflow_2_MiniSeq.sh
@@ -17,3 +17,20 @@ grep -P "^MN00185:260:000H3W2LL:\d:\d+:\d+:\d+\t256\t" L2R1toL1_k$k.sam | cut -f
 
 gethificoord.py L2R1toL1_k$k.hits > one_hit_hifi_reads_nspot2tiles.L 2>n_hits_hifi_reads.L
 # sort -k 2nr one_hit_hifi_reads_nspot2tiles.L
+
+################################################################################################################
+date;
+countreadundancy $L1R1 > Library1_readundancy_check.o 2>Library1_readundancy_check.e;
+date
+if [ -e unique_coord_per_tile.o ]; 
+then 
+echo "Found";
+rm unique_coord_per_tile.o; 
+fi
+
+for j in `gunzip -c $L1R1 | grep "MN00185" | cut -d " " -f 1 | cut -d ":" -f 5 | sort | uniq`;
+do
+tile=FVV:1:$j:
+n=`grep $tile Library1_readundancy_check.o | grep -P "\s1$" | wc -l` 
+echo $tile $n >> unique_coord_per_tile.o
+done
