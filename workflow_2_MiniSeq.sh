27f40328dbba354ffaf11ce6c545ea9a358aa169
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-20 08:32:04 -0700

    Update workflow_2_MiniSeq.sh

--- workflow_2_MiniSeq.sh
+++ workflow_2_MiniSeq.sh
@@ -16,4 +16,4 @@ grep -P "^MN00185:260:000H3W2LL:\d:\d+:\d+:\d+\t0\t" L2R1toL1_k$k.sam | cut -f 1
 grep -P "^MN00185:260:000H3W2LL:\d:\d+:\d+:\d+\t256\t" L2R1toL1_k$k.sam | cut -f 1,2,3 >> L2R1toL1_k$k.hits
 
 gethificoord.py L2R1toL1_k$k.hits > one_hit_hifi_reads_nspot2tiles.L 2>n_hits_hifi_reads.L
-sort -k 2nr one_hit_hifi_reads_nspot2tiles.L
+# sort -k 2nr one_hit_hifi_reads_nspot2tiles.L
