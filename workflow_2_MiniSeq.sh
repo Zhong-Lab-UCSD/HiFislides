b92eecbb89b77a2e0210d25b77dd40cfa77c9933
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-20 08:16:59 -0700

    Update workflow_2_MiniSeq.sh

--- workflow_2_MiniSeq.sh
+++ workflow_2_MiniSeq.sh
@@ -2,6 +2,8 @@
 L1R1=./lib1/Data/Intensities/BaseCalls/Undetermined_S0_L001_R1_001.fastq.gz 
 L2R1=./lib2/Data/Intensities/BaseCalls/Undetermined_S0_L001_R1_001.fastq.gz
 
+# gunzip -c $L1R1 | grep "MN00185" | cut -d " " -f 1 | cut -d ":" -f 5 | sort | uniq
+
 nohup bwa index -p L1R1 $L1R1 > bwaindexo 2>bwaindexe &
 
 k=54
@@ -13,4 +15,5 @@ date
 grep -P "^MN00185:260:000H3W2LL:\d:\d+:\d+:\d+\t0\t" L2R1toL1_k$k.sam | cut -f 1,2,3 > L2R1toL1_k$k.hits
 grep -P "^MN00185:260:000H3W2LL:\d:\d+:\d+:\d+\t256\t" L2R1toL1_k$k.sam | cut -f 1,2,3 >> L2R1toL1_k$k.hits
 
-gethificoord.py L2R1toL1_k$k.hits > one_hit_hifi_reads.L 2>multi_hits_hifi_reads.L
+gethificoord.py L2R1toL1_k$k.hits > one_hit_hifi_reads_nspot2tiles.L 2>n_hits_hifi_reads.L
+sort -k 2nr one_hit_hifi_reads_nspot2tiles.L
