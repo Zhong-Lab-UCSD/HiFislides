676eda68d7821b729e1e61d0bf2dfcb6f315d734
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-04-26 22:14:01 -0700

    Update get_xy_by_clicking.py

--- get_xy_by_clicking.py
+++ get_xy_by_clicking.py
@@ -1,5 +1,6 @@
 import cv2
-  
+# https://www.geeksforgeeks.org/displaying-the-coordinates-of-the-points-clicked-on-the-image-using-python-opencv/
+
 # function to display the coordinates of
 # of the points clicked on the image
 def click_event(event, x, y, flags, params):
@@ -10,13 +11,6 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates
         # on the Shell
         print(x, ' ', y)
- 
-        # displaying the coordinates
-        # on the image window
-        #font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(img, str(x) + ',' +
-         #           str(y), (x,y), font,
-          #          1, (255, 0, 0), 2)
         cv2.imshow('image', img)
  
     # checking for right mouse clicks    
@@ -25,17 +19,6 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates
         # on the Shell
         print(x, ' ', y)
- 
-        # displaying the coordinates
-        # on the image window
-        #font = cv2.FONT_HERSHEY_SIMPLEX
-        #b = img[y, x, 0]
-        #g = img[y, x, 1]
-        #r = img[y, x, 2]
-        #cv2.putText(img, str(b) + ',' +
-         #           str(g) + ',' + str(r),
-          #          (x,y), font, 1,
-           #         (255, 255, 0), 2)
         cv2.imshow('image', img)
  
 # driver function
