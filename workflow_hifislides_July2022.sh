3963e43a5ddeb3cec8c2f8d15bf9734442ea6c08
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-07-20 14:32:49 -0700

    Update workflow_hifislides_July2022.sh

--- workflow_hifislides_July2022.sh
+++ workflow_hifislides_July2022.sh
@@ -2,22 +2,22 @@
 # then the unique raw reads from recycled flow cell would be used as spatial barcodes.
 
 # working dir:
-#
+# /mnt/extraids/OceanStor-0/linpei/hifi/data_12/lib1/raw2 
 
 flowcell=AAAL33WM5
 
-#
-for i in 1 2
-do
-for j in 1 2
-do
+# i=1 means Lane 1
+# j=1 means top surface. (1:top, 2:bottom)
+# both surfdedup and finduniqread.pl are executable programs at /home/linpei/bin
+
+i=1
+j=1
 surf=$flowcell:$i:$j
 date
 surfdedup $surf *_L00$i\_R1_001.fastq.gz > L1R1Dedup_$i$j.fasta
 date
 finduniqread.pl L1R1Dedup_$i$j.fasta > L1R1Uniq_$i$j.fasta
-done
-done
+
 
 
 # working dir:
@@ -30,13 +30,26 @@ flowcell=AAAL33WM5
 seq=L1R1Uniq_11
 k=40
 
+### work with the coordinate end, i.e., R1 of HiFi slides read pairs
 bwa index -p L2R1 $L2R1 > bwaindexL2R1o 2>bwaindexL2R1e
 
-
-k=40
-seq=L1R1Uniq_11
 sam=$seq\_L2R1_ak$k.sam
 bwa mem L2R1 $seq.fasta -a -k $k -t 64 > $sam 2>$seq\_L2R1_ak$k.same
 
 grep -P "\t0\tMN00185:" $sam | cut -f 1,2,3 > $seq\_L2R1_ak$k\_mappedspot.L
 grep -P "\t256\tMN00185:" $sam | cut -f 1,2,3 >> $seq\_L2R1_ak$k\_mappedspot.L
+
+### work with the RNA end, i.e., R2 of HiFi slides read pairs
+# mwd=/mnt/extraids/OceanStor-0/linpei
+hg38=$mwd/imc/HG38
+prefix=L2R2_000_
+STAR --runThreadN 32 --genomeDir $hg38 --readFilesIn $L2R2 --quantMode GeneCounts --readFilesCommand zcat \
+--outFileNamePrefix $prefix \
+--outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 > starlogo 2>starlogoe
+
+# Reference:
+# https://github.com/alexdobin/STAR/issues/415
+# https://github.com/leeju-umich/Cho_Xi_Seqscope/blob/main/script/align.sh 
+
+### 
+
