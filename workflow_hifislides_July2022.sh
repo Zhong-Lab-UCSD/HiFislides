e0459f929e5acbb04a7d3987bb260c39d565a9b1
linpei26 <68634556+linpei26@users.noreply.github.com>
2022-07-20 18:50:48 -0700

    Update workflow_hifislides_July2022.sh

--- workflow_hifislides_July2022.sh
+++ workflow_hifislides_July2022.sh
@@ -3,6 +3,8 @@ i=1
 j=1
 surf=$flowcell:$i:$j
 
+
+
 cd /mnt/extraids/OceanStor-0/linpei/hifi/data_12/lib1/raw2
 surfdedup $surf *_L00$i\_R1_001.fastq.gz > L1R1Dedup_$i$j.fasta
 finduniqread.pl L1R1Dedup_$i$j.fasta > /mnt/extraids/OceanStor-0/linpei/hifi/data_14/lib2/L1R1Uniq_$i$j.fasta
