23c9abfb4b60690bff80e8b75fe601ca82f314a6
Pie <68634556+linpei26@users.noreply.github.com>
2022-10-01 15:31:00 -0700

    Add files via upload

---
+++ pei_various_scripts/ks_test_to_identify_ROI.R
@@ -0,0 +1,44 @@
+data = read.table("Output_by_hifislida2.o",sep="\t")
+
+x = 1101:1111
+
+
+tilm = cbind(x,x+100,x+200,x+300,x+400,x+500)
+
+# tilm is the matrix for arranging Tile IDs as their relative position on the surface
+# we have 11 x 6 tiles.
+
+tile = as.numeric(gsub("T","",data[,2]))
+
+data2 = cbind(tile,data[,2:4])
+
+mat = matrix(data=NA,nrow=1,ncol=6)
+
+# w: the size of ROI. 
+# w = 3 indicate ROI has 3 x 3 tiles
+
+# we do not know the exact shape and location of ROI.
+# so, we rank potential ROIs to see which is most significantly different from the remaining tiles.
+
+for(w in c(2,3,4)) {
+	d = w - 1
+	for(i in 1:(6-d)) {
+		for(j in 1:(11-d)) {
+
+			w1 = which(data2[,1] %in% tilm[j:(j+d),i:(i+d)])
+			w0 = which(!(data2[,1] %in% tilm[j:(j+d),i:(i+d)]))
+			pv = ks.test(data2[w1,3],data2[w0,3])$p.value
+			# print(tilm[j:(j+2),i:(i+2)])
+		
+			mat = rbind(mat,c(d,i,j,mean(data2[w1,3]),mean(data2[w0,3]),pv))
+		}
+	}
+}
+
+mat = mat[-1,]
+mat = mat[order(mat[,ncol(mat)],decreasing=F),]
+i = mat[1,2]
+j = mat[1,3]
+d = mat[1,1]
+# print(tilm[j:(j+d),i:(i+d)])
+
