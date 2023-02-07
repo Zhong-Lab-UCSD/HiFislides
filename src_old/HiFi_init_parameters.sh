89d5bfea216712636c45ba2c6a6249a547369b10
Riccardo Calandrelli <rcalandrelli@eng.ucsd.edu>
2023-02-07 15:08:17 -0800

    Update HiFi_init_parameters.sh

--- src_old/HiFi_init_parameters.sh
+++ src_old/HiFi_init_parameters.sh
@@ -3,12 +3,10 @@ OUT_DIR=/mnt/extraids/SDSC_NFS/rcalandrelli/HiFi/data/IGM
 BIN_DIR=/mnt/extraids/SDSC_NFS/rcalandrelli/HiFi/data/bin
 N_THREADS=32
 
-SAMPLE_NAME=HiFi_placenta_1
+SAMPLE_NAME=HiFi_cortex_1
 
 # Flowcell and surface identifiers
-L1_DIR=/mnt/extraids/SDSC_NFS/rcalandrelli/HiFi/data/barcodes # directory of the spatial barcodes
-flowcell_type="NextSeq" # one of: MiniSeq, NextSeq
-flowcell="AAALN5GM5"
+L1_DIR=/mnt/extraids/SDSC_NFS/rcalandrelli/HiFi/data/barcodes/AAANLCHHV_2_1 # directory of the spatial barcodes
 
 # Raw reads of HiFi Slides sequencing
 L2R1_FASTQ=/mnt/extraids/SDSC_NFS/rcalandrelli/Lab_sequencing/IGM/fastq/221018_A00953_0641_BHGHWNDRX2/JP_placenta_hifi_05oct22_S2_L002_R1_001.fastq.gz
